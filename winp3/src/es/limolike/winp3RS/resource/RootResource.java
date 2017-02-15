fb2fd9065fd6e8048cd9cc4602cb0fc67d373ad3
gimeja49 <gimeja49@BBME2069.bbmag.bbraun.com>
2017-02-15 16:37:29 +0100

    ashgasdjkjk

--- winp3/src/es/limolike/winp3RS/resource/RootResource.java
+++ winp3/src/es/limolike/winp3RS/resource/RootResource.java
@@ -3,9 +3,12 @@ package es.limolike.winp3RS.resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import es.limolike.winp3RS.domain.Simulator;
 
 @Path("/")
 public class RootResource {
@@ -16,5 +19,14 @@ public class RootResource {
 		System.out.println("RootResource: index");
 		return "/page";
 	}
-	
+
+
+	@GET
+	@Path("/generate/{name}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Simulator responseMsg( @PathParam("name") String name ) {
+		Simulator st = new Simulator(1, name);
+		return st;
+	}
+
 }

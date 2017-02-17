8e51759f195a6f27b55df49d744c5d9d76f5e96d
Geraldine Martín Socas <geraldine.ms@me.com>
2017-02-17 12:23:39 +0100

    conflicto

--- winp3/src/es/limolike/winp3RS/domain/Simulator.java
+++ winp3/src/es/limolike/winp3RS/domain/Simulator.java
@@ -1,8 +1,41 @@
 package es.limolike.winp3RS.domain;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement(name = "simulator")
 public class Simulator {
+	private int id;
+	private String Name;
 
+	public Simulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Simulator(int id, String name) {
+		super();
+		this.id = id;
+		Name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@XmlAttribute
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	@XmlElement
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	
 }
